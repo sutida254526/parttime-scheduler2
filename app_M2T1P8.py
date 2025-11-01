5555be794a21d108edb6ee28c1436326cda37237
sutida254526 <sutida.rod@dome.tu.ac.th>
2025-11-01 19:32:08 +0700

    Update app_M2T1P8.py

--- app_M2T1P8.py
+++ app_M2T1P8.py
@@ -109,7 +109,7 @@ def solver_parttime(data):
                 ) <= 1
                 
     for d in D:
-        available = [i for i in I if any(all_employees_avail[i].get(d, []) and 1 in all_employees_avail[i][d])]
+            available = [i for i in I if any(all_employees_avail[i].get(d, []))]
         print(f"Day {d} available for Shift 1:", available)
         
     for d in range(1,7):
